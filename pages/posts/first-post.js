d9c2e14917e1f9eb9ee3bf029f697785fdfedc4c
ynishimura <ynishimura0922@gmail.com>
2021-01-13 22:04:18 +0900

    add layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import Head from 'next/head';
-
+import Layout from '../../components/layout';
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -13,6 +13,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
